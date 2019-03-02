414cd8c444c971ce5b9931e3fc1860e037befd16
Mohit Kumar <mohitkumar97611khola@gmail.com>
2019-03-02 22:54:56 +0530

    adding favorite tag to songs feature is added

--- music/views.py
+++ music/views.py
@@ -15,13 +15,28 @@ def index(request):
 
 def detail(request, album_id ):
     #return HttpResponse("<h1>Details for album id " + str(album_id) +  "</h1>")
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("album Does not exits")
+    #try:
+    #    album = Album.objects.get(pk=album_id)
+    #except Album.DoesNotExist:
+    #    raise Http404("album Does not exits")
+    album = get_object_or_404(Album,pk =album_id ) 
     return render(request, 'music/detail.html',{ 'album': album,})
     #album = get_object_or_404(Album,pk =album_id )
-        
+
+
+def favorite(request ,album_id):
+    album = get_object_or_404(Album,pk =album_id ) 
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError,Song.DoesNotExist):
+        return render(request, 'music/detail.html',{'album':album , 'error_message': "you did not select any Valid Song" })
+    else:
+         if selected_song.is_favorite:
+            selected_song.is_favorite = False
+         else:
+            selected_song.is_favorite = True
+         selected_song.save()
+         return render(request, 'music/detail.html', {'album': album})
 
     
 
